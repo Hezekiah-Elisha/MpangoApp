167aff0cba1b9d68bc2d83a116027c50ffbb0169
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2026-08-03 16:58:41 +0300

    Add BottomNav component and integrate with ApplicationNavigation; update Route interface for navigation

--- app/src/main/java/ke/hub/mpangoapp/navigation/Route.kt
+++ app/src/main/java/ke/hub/mpangoapp/navigation/Route.kt
@@ -3,13 +3,37 @@ package ke.hub.mpangoapp.navigation
 import androidx.navigation3.runtime.NavKey
 import kotlinx.serialization.Serializable
 
+interface BottomNavRoute {
+    val selectedIcon: Int
+    val unselectedIcon: Int
+    val label: String
+}
+
 sealed interface Route : NavKey {
     @Serializable
-    data object Home : Route, NavKey
+    data object Home : Route, BottomNavRoute, NavKey {
+        override val selectedIcon: Int
+            get() = TODO("Not yet implemented")
+        override val unselectedIcon: Int
+            get() = TODO("Not yet implemented")
+        override val label = "Home"
+    }
 
     @Serializable
-    data object Transfer : Route, NavKey
+    data object Transfer : Route, BottomNavRoute, NavKey {
+        override val selectedIcon: Int
+            get() = TODO("Not yet implemented")
+        override val unselectedIcon: Int
+            get() = TODO("Not yet implemented")
+        override val label = "Transfer"
+    }
 
     @Serializable
-    data object Analytics : Route, NavKey
+    data object Analytics : Route, BottomNavRoute, NavKey {
+        override val selectedIcon: Int
+            get() = TODO("Not yet implemented")
+        override val unselectedIcon: Int
+            get() = TODO("Not yet implemented")
+        override val label = "Analytics"
+    }
 }
