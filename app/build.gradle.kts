348fea176a64769fb8e8b769116d6e5b3160343d
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2026-08-07 22:35:34 +0300

    Add SplashScreen dependency and update version in libs.versions.toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,8 @@ room3 {
 
 dependencies {
 
+    implementation(libs.androidx.core.splashscreen)
+
     implementation(libs.androidx.compose.ui.text.google.fonts)
     implementation(libs.androidx.room3.runtime)
     ksp(libs.androidx.room3.compiler)
